227d3a96dd9655e570f41aa7e685da8dde3ae4a8
guilleRodriguez <guillerc3312@gmail.com>
2026-04-28 18:10:42 -0600

    Fix React runtime in production

--- index.jsx
+++ index.jsx
@@ -1,5 +1,4 @@
 // Entry point for React app
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import HomePage from './HomePage';
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  esbuild: {
+    jsxInject: "import React from 'react'",
+  },
   root: '.',
   build: {
     outDir: 'dist',
